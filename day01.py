6c9760ebde3bc5252a941ca2cca9c99dae9f29d4
vidhi udasi <udasividhi2@gmail.com>
2026-01-28 21:47:36 +0530

    Update day01.py

--- day01.py
+++ day01.py
@@ -3,8 +3,9 @@ import pandas as pd
 df=pd.read_csv('IPL2025Batters.csv')
 df = df[['Player Name', 'Runs']]
 st.write(""" 
-# IPL-2026
+# IPL-2025
 Simple Predictor App using StreamLit""")
 st.line_chart(df, x='Player Name', y='Runs')
 st.bar_chart(df, x='Player Name', y='Runs')
 st.scatter_chart(df, x='Player Name', y='Runs',color='#FFC0CB')
+
